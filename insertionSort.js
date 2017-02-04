3718f5a765d94e2987cf67e1cf22cd7d76f428ba
Steven Cable <steven.cable12@gmail.com>
2017-02-03 23:47:47 -1000

    added insertion sort function...i think it works

---
+++ insertionSort.js
@@ -0,0 +1,11 @@
+function insertionSort(array){
+  for (var i = 1; i < array.length; i++) {
+    var holder = array[i];  //placeholder during swap
+    let j = i-1;
+
+    for (j >= 0 && array[j]>holder; j < array.length; j--) {
+      array[j+1] = array[j]; //swap if number on left is smaller
+    }
+    array[j+1] = holder; //if number on left DNE or is smaller replace number on right with placeholder
+  }
+}
