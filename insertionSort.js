a8e0ddb289f779a52ba944fc14a5682ddaddbd1f
Steven Cable <steven.cable12@gmail.com>
2017-02-03 23:49:13 -1000

    updated comments in insertion sort function

--- insertionSort.js
+++ insertionSort.js
@@ -3,7 +3,7 @@ function insertionSort(array){
     var holder = array[i];  //placeholder during swap
     let j = i-1;
 
-    for (j >= 0 && array[j]>holder; j < array.length; j--) {
+    for (j >= 0 && array[j]>holder; j < array.length; j--) {  //second loop (depricating) to sort left while going right)
       array[j+1] = array[j]; //swap if number on left is smaller
     }
     array[j+1] = holder; //if number on left DNE or is smaller replace number on right with placeholder
